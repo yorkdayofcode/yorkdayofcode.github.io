622c9231de4bfabc8bea0bc37ac60f465ac4c8d7
David Betteridge <DavidaBetteridge@gmail.com>
2018-07-04 21:41:49 +0100

    Basic DSL

---
+++ dsl.js
@@ -0,0 +1,87 @@
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/monokai");
+editor.session.setMode("ace/mode/javascript");
+editor.setOptions({
+    fontFamily: "tahoma",
+    fontSize: "20pt"
+  });
+
+function getCanvas() {
+    return document.querySelector("#canvas");
+}
+
+function getCanvasContext(canvasElement) {
+    return canvasElement.getContext("2d");
+}
+ 
+function clearCanvas(canvasElement, context) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function getScript() {
+    var editor = ace.edit("editor");
+    return editor.getValue();
+}
+
+function convertCanvasToImage() {
+    var jpegUrl = canvas.toDataURL("image/jpeg");
+//  document.write(jpegUrl);
+}
+
+function run()
+{
+    var canvasElement = getCanvas();
+    var canvasContext = getCanvasContext(canvasElement);
+
+    clearCanvas(canvasElement, canvasContext);
+    var script = getScript();
+
+    Function(script)()(
+        Triangle, Square 
+    );
+}
+
+function Square() {
+    var canvasElement = getCanvas();
+    var context = getCanvasContext(canvasElement);
+
+    // the square
+    context.beginPath();
+    context.moveTo(100, 0);
+    context.lineTo(100, 100);
+    context.lineTo(200, 100);
+    context.lineTo(200, 0);
+    context.lineTo(100, 0);
+    context.closePath();
+
+    // the outline
+    context.lineWidth = 10;
+    context.strokeStyle = '#666666';
+    context.stroke();
+
+    // the fill color
+    context.fillStyle = "#FFCC00";
+    context.fill();
+}
+
+
+function Triangle() {
+    var canvasElement = getCanvas();
+    var context = getCanvasContext(canvasElement);
+
+    // the triangle
+    context.beginPath();
+    context.moveTo(100, 0);
+    context.lineTo(100, 300);
+    context.lineTo(300, 200);
+    context.closePath();
+
+    // the outline
+    context.lineWidth = 10;
+    context.strokeStyle = '#666666';
+    context.stroke();
+
+    // the fill color
+    context.fillStyle = "#FF0000";
+    context.fill();
+}
