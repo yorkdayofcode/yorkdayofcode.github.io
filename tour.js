b768f434d899e2a227d7c2d6e691ceda61ab4f6b
David Betteridge <DavidaBetteridge@gmail.com>
2018-07-04 21:54:54 +0100

    Hopscotch

---
+++ tour.js
@@ -0,0 +1,33 @@
+  // Define the tour!
+  var tour = {
+    id: "hopscotch-tour",
+    steps: [
+      {
+        title: "Welcome",
+        content: "York Day Of Code",
+        target: "header",
+        placement: "right"
+      },
+      {
+        title: "Code Editor",
+        content: "This is where you are going to write your program.  To start with type in <b>Square()</b>",
+        target: document.querySelector("#editor"),
+        placement: "right"
+      },
+      {
+        title: "Run Code",
+        content: "Once you have written you program,  click the <b>Run Code</b> button to compile and run it.",
+        target: document.querySelector("#compile"),
+        placement: "left"
+      },
+      {
+        title: "Canvas",
+        content: "If all has gone to plan,  then your picture will be shown here.",
+        target: document.querySelector("#canvas"),
+        placement: "left"
+      }
+    ]
+  };
+
+  // Start the tour!
+  hopscotch.startTour(tour);
